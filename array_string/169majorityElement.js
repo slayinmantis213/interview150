e93e73cf7f9ac75c0e1fbac4bd25d6e5b110a929
Joshua Bliek <jbliek213@yahoo.com>
2023-12-08 09:31:51 -0800

    12-8algos

---
+++ array_string/169majorityElement.js
@@ -0,0 +1,71 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+ 
+
+// Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+//mine
+const majorityElement = (nums) => {
+    let max = 0;
+    let majority; 
+    const map = {};
+    for(let i = 0; i < nums.length; i++){
+        if(map.hasOwnProperty(nums[i])){
+            map[nums[i]] += 1;
+        }
+        else{
+            map[nums[i]] = 1;
+        }
+        if(map[nums[i]] > max){
+            max = map[nums[i]];
+            majority = nums[i];
+        }
+    }
+    console.log(map);
+    return majority;
+}
+
+console.log(majorityElement([2,2,1,1,1,2,2]))
+
+//solution
+// var majorityElement = function(nums) {
+//     // Initialize sol and cnt to store the solution and its frequency for respective iterations...
+//     let sol = 0, cnt = 0;
+//     // For every element i in the array...
+//     for(let i = 0; i < nums.length; i++ ) {
+//         // If cnt is equal to zero, update sol as sol = i
+//         if(cnt == 0){
+//             sol = nums[i];
+//             cnt = 1;
+//         }
+//         // If i is equal to candidate, increment cnt...
+//         else if(sol == nums[i]){
+//             cnt++;
+//         }
+//         // Otherwise, decrement cnt...
+//         else{
+//             cnt--;
+//         }
+//     }
+//     // Return & print the solution...
+//     return sol;
+// };
