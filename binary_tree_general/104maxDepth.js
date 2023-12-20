20b1e23c3ef743a5540893787d59d57d29030c64
Joshua Bliek <jbliek213@yahoo.com>
2023-12-20 10:10:56 -0800

    12-20algo

---
+++ binary_tree_general/104maxDepth.js
@@ -0,0 +1,13 @@
+//mine
+
+var maxDepth = function(root) {
+    if(root == null){
+        return 0;
+    }
+
+    let left = maxDepth(root.left)
+    let right = maxDepth(root.right)
+
+    return Math.max(left, right) + 1;
+};
+
