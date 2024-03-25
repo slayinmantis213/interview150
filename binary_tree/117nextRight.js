5d7072d24f97fff2b3ca4506e1cb2f0f2b2bfd0b
Joshua Bliek <jbliek213@yahoo.com>
2024-03-25 10:44:03 -0700

    algos3-25

--- binary_tree/117nextRight.js
+++ binary_tree/117nextRight.js
@@ -23,16 +23,16 @@ var connect = function(root) {
     const q = [root];
     while(q.length > 0){
         const size = q.length;
-        for(let i = 0; i < q.length; i++){
+        for(let i = 0; i < size; i++){
             const node = q.shift();
             if(i < size - 1){
                 node.next = q[0];
             }
             if(node.left){
-                q.append(node.left);
+                q.push(node.left);
             }
             if(node.right){
-                q.append(node.right);
+                q.push(node.right);
             }
         }
     }
