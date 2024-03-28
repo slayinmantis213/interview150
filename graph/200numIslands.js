dd230d5ff2ae091bf2e031afcdedab45f3ead597
Joshua Bliek <jbliek213@yahoo.com>
2024-03-28 13:23:06 -0700

    3-28algo

--- graph/200numIslands.js
+++ graph/200numIslands.js
@@ -3,44 +3,58 @@
 // An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
 
-var numIslands = function(grid) {
-    let is = 0;
-    const map = new Map();
-    for(let i = 0; i < grid.length; i++){
-        for(let j = 0; j < grid[0].length; j++){
-            if(grid[i][j] === "1"){
-                is++;
-                checkRight(j,i);
-                checkDown(j,i);
+var numIslands = function (grid) {
+    const m = grid.length;
+    const n = grid[0].length;
+    const directions = [
+        [0, 1],
+        [1, 0],
+        [0, -1],
+        [-1, 0],
+    ];
+
+    var dfs = function (grid, row, col) {
+        for (const direction of directions) {
+            const nextRow = row + direction[0];
+            const nextCol = col + direction[1];
+
+            if (
+                0 <= nextRow &&
+                nextRow < m &&
+                0 <= nextCol &&
+                nextCol < n &&
+                grid[nextRow][nextCol] == "1"
+            ) {
+                grid[nextRow][nextCol] = "0";
+                dfs(grid, nextRow, nextCol);
             }
-        } 
+        }
+    };
+
+    let is = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === "1") {
+                is++;
+                dfs(grid, i, j);
+            }
+        }
     }
+    return is;
 };
 
-var checkRight = function(x, y){
-    while(grid[y][x] === "1"){
-        grid[y][x] === "0";
-        x++;
-    }
-}
-var checkDown = function(x, y){
-    while(grid[y][x] === "1"){
-        grid[y][x] === "0";
-        y++;
-    }
 
-}
 
 const grid1 = [
-    ["1","1","1","1","0"],
-    ["1","1","0","1","0"],
-    ["1","1","0","0","0"],
-    ["0","0","0","0","0"]
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"]
 ]
 
 const grid2 = [
-    ["1","1","0","0","0"],
-    ["1","1","0","0","0"],
-    ["0","0","1","0","0"],
-    ["0","0","0","1","1"]
+    ["1", "1", "0", "0", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "1", "0", "0"],
+    ["0", "0", "0", "1", "1"]
 ]
