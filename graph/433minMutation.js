6dc473280406a8fc49929151b7a647c82954b9bd
Joshua Bliek <jbliek213@yahoo.com>
2024-04-15 10:46:51 -0700

    4-15algo

--- graph/433minMutation.js
+++ graph/433minMutation.js
@@ -18,6 +18,7 @@
 @param {string[]} bank
 @return {number}
 */
+
 var minMutation = function (startGene, endGene, bank) {
     const graph = new Map(); // key is string, and value is neighbors
     graph.set(startGene, new Set());
@@ -30,43 +31,49 @@ var minMutation = function (startGene, endGene, bank) {
     if (!graph.has(endGene)) return -1;
 
     // populate the neighbors
-    const splits = []
+    const splits = [];
     for (const mutation of graph.keys()) {
         const split = mutation.split("");
         splits.push(split);
     }
     for (let i = 0; i < splits.length; i++) {
         for (let j = i + 1; j < splits.length; j++) {
-            const count = compareStrings(splits[i], splits[j])
+            const count = compareStrings(splits[i], splits[j]);
             if (count !== 1) {
                 continue;
             }
-            graph.get(splits[i].join('')).add(splits[j].join(''))
-            graph.get(splits[j].join('')).add(splits[i].join(''))
+            graph.get(splits[i].join("")).add(splits[j].join(""));
+            graph.get(splits[j].join("")).add(splits[i].join(""));
         }
     }
     if (graph.get(startGene).has(endGene)) {
         return 1;
     }
+
     let count = 0;
     const q = [startGene];
     const visited = new Set();
+    visited.add(startGene);
     while (q.length > 0) {
-        //prolly some logic in here
-        let curr = q.shift()
-        if(visited.has(curr)){
-            continue;
-        }
-        count++;
-        visited.add(curr);
-
-        curr = graph.get(curr)
-        for (const mutation of curr.keys()) {
-            q.push(mutation)
-            if (mutation === endGene) {
+        let len = q.length;
+        for (let i = 0; i < len; i++) {
+            let curr = q.shift();
+            if (curr === endGene) {
                 return count;
             }
+
+            const node = graph.get(curr);
+            for (const mutation of node.keys()) {
+                if (visited.has(mutation)) continue;
+                // if (mutation === endGene) {
+                //   return count;
+                // }
+                // console.log(mutation);
+                q.push(mutation);
+                visited.add(mutation);
+            }
         }
+        count++;
     }
     return -1;
 };
@@ -82,7 +89,7 @@ const compareStrings = (splits1, splits2) => {
         }
     }
     return count;
-}
+};
 
 const startGene = "AACCGGTT", endGene = "AAACGGTA", bank = ["AACCGGTA", "AACCGCTA", "AAACGGTA"]
 
@@ -95,3 +102,13 @@ const startGene1 =
         ["AAAACCCC", "AAACCCCC", "AACCCCCC"]
 
 console.log(minMutation(startGene1, endGene1, bank1))
+
+var stepsToEndFromStart = function (startGene, endGene) {
+    const startSplit = startGene.split("");
+    const endSplit = endGene.split("");
+    let count = 0;
+    for (let i = 0; i < startSplit.length; i++) {
+        startSplit[i] !== endSplit[i] && count++;
+    }
+    return count;
+};
