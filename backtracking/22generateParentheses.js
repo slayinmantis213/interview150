9caab197c0c91e5bfd65781c8aef94b71903be8f
Joshua Bliek <jbliek213@yahoo.com>
2024-05-03 10:35:50 -0700

    5-3algo

--- backtracking/22generateParentheses.js
+++ backtracking/22generateParentheses.js
@@ -25,86 +25,18 @@
  * @param {number} n
  * @return {string[]}
  */
-
-var generateParenthesis = function (n) {
-    const path = new Array(n * 2).fill("(", 0, n).fill(")", n, n * 2);
-    const res = [];
-    const visited = new Set();
-    const dfs = (path, permutation) => {
-        if (permutation.length === path.length) {
-            let open = 0;
-            permutation = permutation.join("");
-            if(visited.has(permutation)){
-                return;
-            }
-            visited.add(permutation);
-            for(let i = 0; i < path.length; i++){
-                if(permutation.charAt(i) === "("){
-                    open ++;
-                }
-                else if(permutation.charAt(i) === ")"){
-                    open --;
-                }
-                if(open < 0 || open > n){
-                    return;
-                }
-            }
-            if(open !== 0) return
-            res.push(permutation);
-            return;
-        }
-        for (let i = 0; i < path.length; i++) {
-            permutation.push(path[i]);
-            dfs(path, permutation);
-            permutation.pop();
-        }
-    };
-    dfs(path, []);
+function generateParenthesis(n) {
+    let res = [];
+    generate("", n, n, res);
     return res;
-};
-// var generateParenthesis = function (n) {
-//     const path = new Array(n * 2).fill("(", 0, n).fill(")", n, n * 2);
-//     const res = [];
-//     const visited = new Set();
-//     const dfs = (path, permutation) => {
-//         if (permutation.length === path.length) {
-//             res.push(permutation.join(""));
-//             return;
-//         }
-        
-//         const perm = permutation.join("");
-//         for (let i = 0; i < path.length; i++) {
-//             // console.log(i)
-//             let open = 0;
-//             if (permutation.length === path.length) {
-                
-//                 for(const char of permutation){
-//                     if(char === "("){
-//                         open ++;
-//                     }
-//                     else if(char === ")"){
-//                         open --;
-//                     }
-//                     if(open < 0 || open > n){
-//                         permutation.pop();
-//                         return;
-//                     }
-//                 }
-//             }
-//             permutation.push(path[i]);
-//             if(visited.has(perm)){
-//                 permutation.pop();
-//                 return;
-//             }
-//             visited.add(perm);
-//             console.log(permutation)
-//             dfs(path, permutation);
-//             permutation.pop();
-//         }
-//     };
-//     dfs(path, []);
-//     console.log(res)
-//     return res;
-// };
-//   console.log("ans")
-  console.log(generateParenthesis(5));
+}
+function generate(str, left, right, res) {
+    if (!left && !right && str.length) {
+        res.push(str);
+        return;
+    }
+    if (left) generate(str + "(", left - 1, right, res);
+    if (right > left) generate(str + ")", left, right - 1, res);
+}
+
+  console.log(generateParenthesis(10));
