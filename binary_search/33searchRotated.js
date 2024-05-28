92f5bf7100d9f83f04664e3f8ff0f06c204b04d9
Joshua Bliek <jbliek213@yahoo.com>
2024-05-28 10:28:28 -0700

    5-28 algo

---
+++ binary_search/33searchRotated.js
@@ -0,0 +1,31 @@
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid; // Target found
+        }
+
+        // Determine which side is normally ordered
+        if (nums[left] <= nums[mid]) {
+            // Left side is normally ordered
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1; // Target is in the left half
+            } else {
+                left = mid + 1; // Target is in the right half
+            }
+        } else {
+            // Right side is normally ordered
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1; // Target is in the right half
+            } else {
+                right = mid - 1; // Target is in the left half
+            }
+        }
+    }
+
+    return -1; // Target not found
+};
